6961738e292effdaaec08d76a5e4d19496bcdd5d
Rishabh455 <94906188+Rishabh455@users.noreply.github.com>
2023-02-10 18:15:27 +0530

    litos

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -80,7 +80,7 @@ export default function Signup() {
     <div
       style={{
         backgroundImage:
-          'url("https://images.pexels.com/photos/1565982/pexels-photo-1565982.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
+          'url("https://images.pexels.com/litos/1565982/pexels-photo-1565982.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
         backgroundSize: "cover",
         height: "100vh",
       }}
